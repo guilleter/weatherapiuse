7ae43194af20da44d659007b0b99b74fd239cf92
guilleter <guille.theisen@gmail.com>
2023-12-13 19:15:56 -0300

    agrega backend y consumo de api

---
+++ index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import express from "express";
+
+const app=express();
+const port=3000;
+const config={
+    params: {
+        q: '-27.36,-55.89'
+    },
+    headers: {
+        'X-RapidAPI-Key': '03c7c135d3msh66da49927685768p1fc252jsn88d473a7222d',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+    }
+}
+
+app.get("/",async(req,res)=>{
+    const response= await axios.get("https://weatherapi-com.p.rapidapi.com/current.json",config);
+    console.log(response.data);
+    res.render("index.ejs",{temp:response.data.current.temp_c,imagen:response.data.current.condition.icon});
+    console.log("arrancamo");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
